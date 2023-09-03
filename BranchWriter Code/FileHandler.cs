c69b3371fa2446f7ce2e6d7468ee5ac3adc8df3a
Jamillll <mjmister2006@outlook.com>
2023-09-03 19:07:59 +0100

    file selector functionality completed
    all of the functionality for dropping files into the file selector, opening a file explorer with the file selector and a few basic error messages are completed. Still will have to return to this later to make it look nicer, especially the error messages

--- BranchWriter Code/FileHandler.cs
+++ BranchWriter Code/FileHandler.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BranchWriter_Code
 {
     internal static class FileHandler
     {
         public static string? path = null;
+
+        public static bool IsLeaf(string checkPath)
+        {
+            if (checkPath.Split('.')[^1] == "leaf") return true;
+            else return false;
+        }
     }
 }
--- BranchWriter Code/SelectFile.xaml.cs
+++ BranchWriter Code/SelectFile.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +38,49 @@ namespace BranchWriter_Code
         private void CloseButton(object sender, RoutedEventArgs e)
         {
             System.Environment.Exit(0);
+        }
+
+        private void FileDropped(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+
+                if (FileHandler.IsLeaf(filePaths[0]))
+                {
+                    FileHandler.path = filePaths[0];
+                }
+
+                else
+                {
+                    MessageBox.Show("Please only drop in leaf files.");
+                    return;
+                }
+            }
+
+            this.Close();
+
+        }
+
+        private void FileOpenerClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileExplorer = new OpenFileDialog();
+            fileExplorer.Multiselect = false;
+
+            if (fileExplorer.ShowDialog() == true)
+            {
+                if (FileHandler.IsLeaf(fileExplorer.FileName))
+                {
+                    FileHandler.path = fileExplorer.FileName;
+                    this.Close();
+                }
+
+                else
+                {
+                    MessageBox.Show("Please select one leaf file");
+                    return;
+                }
+            }
         }
     }
 }
